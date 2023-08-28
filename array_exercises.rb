3cd76fd81c47a05588864de3f86d3ea0eb818ec7
Alejandro Artola <aartolam@fwdcostarica.com>
2023-08-28 13:03:37 -0600

    terminado

---
+++ array_exercises.rb
@@ -0,0 +1,92 @@
+# def nil_array(number)
+#   number.times do
+#     puts "nill"
+#   end
+#   end
+
+#    nil_array(6)
+  
+  # def first_element(array)
+  #  puts array[0]
+  # end
+
+  # first_element(["hola", "adios"])
+  
+  # def third_element(array)
+  #   puts array[2]
+  # end
+  # third_element([1, 2, 3, 4, 5, 6])
+  
+  # def last_three_elements(array)
+  #   puts array.pop(3)
+  # end
+
+  # last_three_elements([1, 2, 3, 4, 5])
+  
+  # def add_element(array)
+  #   lista = [1, 2, 3,4 ]
+
+  #   puts lista.push(array)
+  # end
+
+  # add_element([5, 6])
+  
+  # def remove_last_element(array)
+  #  newArray = array.shift()
+  #  puts array 
+  # end
+
+  # remove_last_element([1, 2, 3, 4, 5])
+  
+  # def remove_first_three_elements(array)
+  #   newArray = array.shift(3)
+  #    puts array 
+  
+    
+    
+  # end
+  # remove_first_three_elements([1, 2, 3, 4, 5])
+  
+  # def array_concatenation(original, additional)
+  #   puts original | additional
+  # end
+
+  # array_concatenation([1,2,3], [4, 5, 6])
+  
+  # def array_difference(original, comparison)
+  # puts original & comparison
+  # end
+
+  
+  # array_difference([1,2,3, 3,3], [4, 5, 6, 3, 3])
+  
+  # def empty_array?(array)
+  #   puts array.empty?
+  # end
+
+  # empty_array?([])
+
+  
+  # def reverse(array)
+  #  puts array.reverse
+  # end
+
+  # reverse([1, 2, 3, 4])
+  
+  # def array_length(array)
+  #  puts array.length
+  # end
+
+  # array_length([1, 2, 3, 4])
+  
+  # def include?(array, value)
+  #   puts array.include?(value)
+  # end
+
+  # include?([1,2,3,4], 2)
+  
+  # def join(array, separator)
+  #   puts array.join(separator)
+  # end
+
+  # join([1,2,3,4], "-")
